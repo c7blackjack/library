1371eac403d779160e8389ba0f80ac1b7696a521
TravisLloyd <jackson231@Traviss-iMac.local>
2018-09-18 00:00:19 -0700

    init

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dists')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.render('index');
 });
 
-app.listen(3000, () => {
-  debug(chalk.green('Listening on port 3000'));
+app.listen(port, () => {
+  debug(`Listening on port ${chalk.green(port)}`);
 });
