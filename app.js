5ab55a5398e144c147bfb0b60945b87484a48a90
TravisLloyd <jackson231@Traviss-iMac.local>
2018-09-26 09:06:58 -0700

    Created and connected to MongoDB/appLibrary

--- app.js
+++ app.js
@@ -4,27 +4,43 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+/* Styling paths being declared */
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dists')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+const nav = [
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'author' }
+];
+
+/* Bringing in and using our books route from src/routes/bookRoutes */
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
+
+
+app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', (req, res) => {
   res.render(
     'index',
     {
-      nav: ['books', 'author'],
+      nav,
       title: 'My Library'
     }
   );
 });
 
+/* Declaring our port that should be locally listening on */
 app.listen(port, () => {
   debug(`Listening on port ${chalk.green(port)}`);
 });
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:bookRoutes');
+
+const bookRouter = express.Router();
+
+function router(nav) {
+  bookRouter.route('/')
+    .get((req, res) => {
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url, { useNewUrlParser: true });
+          debug('Connected to server');
+
+          const db = client.db(dbName);
+
+          const col = await db.collection('books');
+
+          const books = await col.find().toArray();
+
+          res.render(
+            'bookListView',
+            {
+              nav,
+              title: 'My Library',
+              books
+            }
+          );
+        } catch (err) {
+          debug(err.stack);
+        }
+        client.close();
+      }());
+    });
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      res.render(
+        'bookView',
+        {
+          nav,
+          title: 'My Library',
+          books: books[id]
+        }
+      );
+    });
+  return bookRouter;
+}
+
+
+module.exports = router;
